ae619d423404c145117b2bcc2ee45db00f42ccf1
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-07-18 16:19:02 +0300

    Add project files.

---
+++ Casgem.ApiLayer/Program.cs
@@ -0,0 +1,35 @@
+using Casgem.BusinessLayer.Abstract;
+using Casgem.BusinessLayer.Concrete;
+using Casgem.DataAccessLayer.Abstract;
+using Casgem.DataAccessLayer.Concrate;
+using Casgem.DataAccessLayer.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddScoped<ICategoryDal,EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService,CategoryManager>();
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
